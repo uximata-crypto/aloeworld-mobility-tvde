cdf83029faeeea6df81b36d369e10fd5b67d31cf
uximata-crypto <uximata@gmail.com>
2026-06-07 10:07:21 +0100

    Delete api/check-env.js

--- api/check-env.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    RESEND_API_KEY: !!process.env.RESEND_API_KEY,
-    EMAIL_FROM: !!process.env.EMAIL_FROM,
-    ADMIN_EMAIL: !!process.env.ADMIN_EMAIL,
-    environment: process.env.VERCEL_ENV || "unknown"
-  });
-}
